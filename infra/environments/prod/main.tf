b78c0d461a47c87688791e47566488e1be44182f
Abraham Joseph <abrahamjoseph@mac.home>
2026-01-11 23:29:03 -0500

    lakeformation data access

--- infra/environments/prod/main.tf
+++ infra/environments/prod/main.tf
@@ -129,3 +129,102 @@ resource "aws_lakeformation_permissions" "analyst_curated_events_read" {
     name          = "curated_events"
   }
 }
+
+data "aws_caller_identity" "current" {}
+
+resource "aws_lakeformation_data_lake_settings" "this" {
+  admins = [
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/terraform-platform-admin",
+    "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/lakeformation-admin"
+  ]
+}
+
+
+data "aws_iam_role" "lf_data_access" {
+  name = "AWSServiceRoleForLakeFormationDataAccess"
+}
+
+resource "aws_lakeformation_resource" "curated_location" {
+  # Use the bucket ARN (not s3://)
+  arn = "arn:aws:s3:::${module.curated_bucket.bucket_name}"
+
+  # LF mode (NOT hybrid)
+  hybrid_access_enabled = false
+
+  # Use the LF service-linked role for data access orchestration
+  use_service_linked_role = true
+
+  # Optional but recommended: ensure LF settings applied first
+  depends_on = [aws_lakeformation_data_lake_settings.this]
+}
+
+resource "aws_lakeformation_permissions" "analyst_curated_location_access" {
+  principal   = module.athena_analyst_role.role_arn
+  permissions = ["DATA_LOCATION_ACCESS"]
+
+  data_location {
+    arn = aws_lakeformation_resource.curated_location.arn
+  }
+}
+
+resource "aws_lakeformation_permissions" "tf_admin_curated_db" {
+  principal   = "arn:aws:iam::195343369122:role/terraform-platform-admin"
+  permissions = ["DESCRIBE"]
+
+  database {
+    name = "curated_prod"
+  }
+}
+
+resource "aws_lakeformation_permissions" "tf_admin_raw_db" {
+  principal   = "arn:aws:iam::195343369122:role/terraform-platform-admin"
+  permissions = ["DESCRIBE"]
+
+  database {
+    name = "raw_prod"
+  }
+}
+
+
+############################################
+# Lake Formation: Table-level permissions
+############################################
+
+resource "aws_lakeformation_permissions" "analyst_curated_events_table" {
+  principal = module.athena_analyst_role.role_arn
+
+  permissions = [
+    "DESCRIBE",
+    "SELECT"
+  ]
+
+  table {
+    database_name = "curated_prod"
+    name          = "curated_events"
+  }
+}
+
+############################################
+# Lake Formation: Column-level permissions
+############################################
+
+resource "aws_lakeformation_permissions" "analyst_curated_events_columns" {
+  principal = module.athena_analyst_role.role_arn
+
+  permissions = ["SELECT"]
+
+  table_with_columns {
+    database_name = "curated_prod"
+    name          = "curated_events"
+    column_names = [
+      "event_time",
+      "event_type",
+      "event_date"
+    ]
+  }
+
+  depends_on = [
+    aws_lakeformation_permissions.analyst_curated_events_table
+  ]
+}
+
